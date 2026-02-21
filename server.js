1e14c2b8f7c2341747000a31cb5c994b92b44232
AdventureSeeker423 <justintdavis99@gmail.com>
2026-02-20 20:40:37 -0500

    adjusted routes

--- server.js
+++ server.js
@@ -158,6 +158,15 @@ function requireGlobalAdmin(req, res, next) {
   next();
 }
 
+function requireAnyAdmin(req, res, next) {
+  const user = req.authentikUser;
+  if (!user || (!user.isGlobalAdmin && !user.isAgencyAdmin)) {
+    const username = user && user.username ? user.username : "";
+    return res.status(403).render("access-denied", { username });
+  }
+  next();
+}
+
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
@@ -219,6 +228,10 @@ app.get("/", (req, res) => {
   return res.redirect("dashboard");
 });
 
+app.get("/users/manage", (req, res) => {
+  const pendingUserRequestsCount = userRequestsSvc.countRequestsForUser(req.authentikUser);
+  return res.render("users-manage", { pendingUserRequestsCount });
+});
 app.get("/users/create", (req, res) => res.render("users-create"));
 app.get("/groups", (req, res) => res.render("groups"));
 app.get("/agencies", requireGlobalAdmin, (req, res) =>
@@ -274,7 +287,7 @@ app.get("/request-access/confirmation", (req, res) => {
 });
 
 // Admin: review pending access requests
-app.get("/pending-user-requests", requireGlobalAdmin, (req, res) => {
+app.get("/pending-user-requests", requireAnyAdmin, (req, res) => {
   return res.render("pending-user-requests");
 });
 
